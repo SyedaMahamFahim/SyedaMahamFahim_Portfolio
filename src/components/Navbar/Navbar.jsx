d8f9a79daa3bdbe34ec3d80810ea680b463128c9
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-03-02 03:04:13 +0500

    edited navgiation and navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,3 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-/* eslint-disable jsx-a11y/anchor-has-content */
-
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
